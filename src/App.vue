72b7048ed108da3974d7c3f32c7135aaef3c955b
Dawid Kuprowski <dawidkuprowski@gmail.com>
2026-01-29 00:09:10 +0100

    Responsive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<main class="w-full min-h-dvh flex flex-col justify-center items-center gap-8">
-		<h1 class="text-8xl text-slate-800 font-semibold">Portfolio</h1>
-		<h2 class="text-3xl text-slate-500 font-light">Strona w budowie...</h2>
+		<h1 class="text-7xl lg:text-8xl text-slate-800 font-semibold">Portfolio</h1>
+		<h2 class="text-2xl lg:text-3xl text-slate-500 font-light">Strona w budowie...</h2>
 		<span class="text-indigo-500 font-light">dawidkuprowski.pl</span>
 	</main>
 </template>
